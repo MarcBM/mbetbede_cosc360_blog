f520cc4b3ded9cd4e794fbb8fd53d5143d1253e1
Marc Betbeder-Matibet <marc.betbeder@hotmail.com>
2024-07-21 21:48:47 +1000

    Implemented PostsController as a resource controlller
    This simply adds all the routes required for posts. Create, Read, Update, Delete.

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'index']) -> name('posts.index');
+Route::resource('posts', PostController::class);
