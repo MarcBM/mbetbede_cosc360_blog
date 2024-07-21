b9bff73e963c29916448fdc4b2d5017eda6f7aa9
Marc Betbeder-Matibet <marc.betbeder@hotmail.com>
2024-07-21 22:55:30 +1000

    Create Functionality

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,7 +29,13 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        Post::create($request->all());
+        return redirect()->route('posts.index');
     }
 
     /**
---
+++ resources/views/Posts/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+  <h1>Write New Post</h1>
+  <a href="{{ url()->previous() }}" class="btn btn-primary">Back</a>
+  <form action="{{ route('posts.store') }}" method="POST">
+    @csrf
+    <div class="mb-3">
+      <label for="title" class="form-label">Title</label>
+      <input type="text" class="form-control" id="title" name="title" required>
+    </div>
+    <div class="mb-3">
+      <label for="content" class="form-label">Content</label>
+      <textarea class="form-control" id="content" name="content" rows="3" required></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+@endsection
