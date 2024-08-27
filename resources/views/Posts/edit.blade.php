190891d18329a9e2763ba64201483f73c5bb1704
Marc Betbeder <marc.betbeder@hotmail.com>
2024-08-27 20:08:53 +1000

    New Routes Defined
    Admin and User Routes defined preparing for new functionality.
    Views altered to use the baseline dashboard as homescreen.

--- resources/views/Posts/edit.blade.php
+++ resources/views/Posts/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
   <h1>Edit Post: {{ $post->title }}</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -8,9 +11,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('posts', PostController::class);
-    Route::get('/home', [App\Http\Controllers\PostController::class, 'index'])->name('home');
+    Route::get('/user/profile', [UserController::class, 'profile']);
+    Route::get('/user/editProfile', [UserController::class, 'editProfile']);
+    Route::put('/user/updateProfile', [UserController::class, 'updateProfile']);
 });
+
+Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/admin/posts', [AdminController::class, 'posts']);
+Route::get('/admin/users', [AdminController::class, 'users']);
+Route::get('/user/{id}/edit', [UserController::class, 'editUser']);
+Route::delete('/user/{id}/delete', [UserController::class, 'deleteUser']);
+
 
 Auth::routes();
