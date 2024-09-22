4c86aa6c8f3309c85800a650dd98c2b358c1d83b
Marc Betbeder <marc.betbeder@hotmail.com>
2024-09-22 18:42:20 +1000

    Post index api implemented
    Get all posts implemented and tested with postman

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index() {
+        $posts = Post::all();
+
+        return response()->json([
+            'data' => $posts,
+            'message' => 'Retrieved successfully',
+        ], 200);
+    }
 }
