a29aa052d4716332f163db06277b8463c4d70d32
Marc Betbeder <marc.betbeder@hotmail.com>
2024-09-22 18:38:27 +1000

    Added Routes for API/Posts
    Route added for api/posts, api/posts/{id}
    
    API/PostController made

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', [UserController::class, 'index'])->middleware('auth:sanctum');
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/posts', [PostController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/posts/{id}', [PostController::class, 'show'])->middleware('auth:sanctum');
