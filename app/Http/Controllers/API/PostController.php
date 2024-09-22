9fa899af1891185e993d979a118aa2bf61a79a02
Marc Betbeder <marc.betbeder@hotmail.com>
2024-09-22 21:27:12 +1000

    Made posts order by creation date

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-        $posts = Post::all();
+        $posts = Post::orderBy('created_at', 'desc')->get();
 
         return response()->json([
             'data' => $posts,
